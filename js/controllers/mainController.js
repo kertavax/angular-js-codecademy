33f930937fe4de57ec44cc14b147732dab831d37
Egill Runar Vidarsson <kertavax@gmail.com>
2016-06-04 10:38:18 -0400

    the course continues

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,18 +1,22 @@
-app.controller('MainController', ['$scope', function($scope) { 
-  $scope.title = 'Top Sellers in Books'; 
-  $scope.author = "Stephen King";
-  $scope.products = [ 
-	  { 
-	    name: 'The Book of Trees', 
-	    price: 19, 
-	    pubdate: new Date('2014', '03', '08'), 
-	    cover: 'img/the-book-of-trees.jpg' 
-	  }, 
-	  { 
-	    name: 'Program or be Programmed', 
-	    price: 8, 
-	    pubdate: new Date('2013', '08', '01'), 
-	    cover: 'img/program-or-be-programmed.jpg' 
-	  } 
+app.controller('MainController', ['$scope', function($scope) {
+	$scope.apps = [
+		{
+			icon: 'img/move.jpg',
+			title: 'MOVE',
+			developer: 'MOVE, Inc.',
+			price: 0.99
+		},
+		{
+			icon: 'img/shutterbugg.jpg',
+			title: 'Shutterbugg',
+			developer: 'Chico Dusty',
+			price: 2.99
+		},
+		{
+			icon: 'img/gameboard.jpg',
+			title: 'Gameboard',
+			developer: 'Armando P.',
+			price: 1.99
+		}
 	]
 }]);
